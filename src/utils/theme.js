7492c57da4e8d6a2171a98dc66a8935b698ed81b
Brian Morang'a <misochobrian@gmail.com>
2021-12-16 15:57:40 +0300

    Adds theme

---
+++ src/utils/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const themeOptions = createTheme({
+    palette: {
+      type: "light",
+      primary: {
+        main: "#059130",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+    },
+});
